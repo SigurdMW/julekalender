1c82c31237ba83eee1f953f9bc6b07aad76b2889
Sigurd Wahl <sigurd.wahl@knowit.no>
2017-11-09 14:11:29 +0100

    updated heading

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render(){
     return (
       <div className="app">
+        <h1>Julekalender</h1>
         <canvas id="sky"></canvas>
         {this.props.children}
       </div>
