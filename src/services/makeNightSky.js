00827da30f9046f3867c4da46451c3043647dc23
Sigurd Wahl <sigurd.wahl@knowit.no>
2017-12-04 15:00:24 +0100

    started on shooting star functionality

--- src/services/makeNightSky.js
+++ src/services/makeNightSky.js
@@ -24,4 +24,27 @@ export function makeNightSky() {
     ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
 	  ctx.fillRect(randomX, randomY, randomSize, randomSize);
 	}
+	
+	// Create shooting star suddenly
+	setInterval(function(){
+		
+		const randTime = (Math.random() * 10) * 2000;
+		setTimeout(function(){
+			console.log(randTime);
+			createShootingStar(ctx, xMax, yMax);
+		}, randTime);
+
+	}, 2000);
+}
+
+function createShootingStar(ctx, xMax, yMax){
+	var randomX = Math.floor((Math.random()*xMax)+1);
+	var randomY = Math.floor((Math.random()*yMax)+1);
+	var randomSize = Math.floor((Math.random()*2)+1);
+	var randomOpacityOne = Math.floor((Math.random()*9)+1);
+	var randomOpacityTwo = Math.floor((Math.random()*9)+1);
+	var randomHue = Math.floor((Math.random()*360)+1);
+
+	ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
+	ctx.fillRect(randomX, randomY, randomSize, randomSize);
 }
