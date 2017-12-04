8d53b2afd44744fde0909510ea18281d8c3fdb31
Sigurd Wahl <sigurd.wahl@knowit.no>
2017-12-04 15:53:51 +0100

    shooting star logic

--- src/services/makeNightSky.js
+++ src/services/makeNightSky.js
@@ -17,34 +17,111 @@ export function makeNightSky() {
 	  var randomHue = Math.floor((Math.random()*360)+1);
 
 		if(randomSize>1) {
-      ctx.shadowBlur = Math.floor((Math.random()*15)+5);
-      ctx.shadowColor = "white";
-	  }
+			ctx.shadowBlur = Math.floor((Math.random()*15)+5);
+			ctx.shadowColor = "white";
+		}
 
     ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
 	  ctx.fillRect(randomX, randomY, randomSize, randomSize);
 	}
 	
 	// Create shooting star suddenly
-	setInterval(function(){
+	// setInterval(function(){
 		
-		const randTime = (Math.random() * 10) * 2000;
-		setTimeout(function(){
-			console.log(randTime);
-			createShootingStar(ctx, xMax, yMax);
-		}, randTime);
+	// 	const randTime = (Math.random() * 10) * 2000;
+	// 	setTimeout(function(){
+	// 		console.log(randTime);
+	// 		createShootingStar(ctx, xMax, yMax);
+	// 	}, randTime);
 
-	}, 2000);
+	// }, 2000);
+
+	createShootingStar();
 }
 
-function createShootingStar(ctx, xMax, yMax){
-	var randomX = Math.floor((Math.random()*xMax)+1);
-	var randomY = Math.floor((Math.random()*yMax)+1);
-	var randomSize = Math.floor((Math.random()*2)+1);
-	var randomOpacityOne = Math.floor((Math.random()*9)+1);
-	var randomOpacityTwo = Math.floor((Math.random()*9)+1);
-	var randomHue = Math.floor((Math.random()*360)+1);
+// function createShootingStar(ctx, xMax, yMax){
+// 	var randomX = Math.floor((Math.random()*xMax)+1);
+// 	var randomY = Math.floor((Math.random()*yMax)+1);
+// 	var randomSize = 10;
+// 	var randomOpacityOne = Math.floor((Math.random()*9)+1);
+// 	var randomOpacityTwo = Math.floor((Math.random()*9)+1);
+// 	var randomHue = Math.floor((Math.random()*360)+1);
 
-	ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
-	ctx.fillRect(randomX, randomY, randomSize, randomSize);
+// 	ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
+// 	ctx.fillRect(randomX, randomY, randomSize, randomSize);
+	
+// 	ctx.beginPath();
+
+// 	for(let i = 0;i < 10; i++){
+// 		ctx.moveTo(3*i, 5*i);
+// 	}
+// }
+
+function createShootingStar(){
+	const canvas = document.createElement("canvas");
+	canvas.height = window.screen.availHeight;
+	canvas.width = window.screen.availWidth;
+	document.body.appendChild(canvas);
+
+	const context = canvas.getContext("2d");
+
+	var requestAnimationFrame =  
+	window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	function(callback) {
+	  return setTimeout(callback, 1);
+	};
+
+	var square = {
+		'x': 50,
+		'y': 50,
+		'width': 100,
+		'height': 100,
+		'fill': '#ffffff'
+	};
+	
+	var render = function() {
+		// Clear the canvas
+		context.clearRect(0, 0, canvas.width, canvas.height);
+	
+		// Draw the square
+		context.beginPath();
+		context.rect(square.x, square.y, square.width, square.height);
+		context.fillStyle = square.fill;
+		context.fill();
+	
+		// Redraw
+		requestAnimationFrame(render);
+	};
+	
+	// Start the redrawing process
+	render();
+
+	var animate = function(prop, val, duration) {
+		// The calculations required for the step function
+		var start = new Date().getTime();
+		var end = start + duration;
+		var current = square[prop];
+		var distance = val - current;
+	  
+		var step = function() {
+		  // Get our current progres
+		  var timestamp = new Date().getTime();
+		  var progress = Math.min((duration - (end - timestamp)) / duration, 1);
+	  
+		  // Update the square's property
+		  square[prop] = current + (distance * progress);
+	  
+		  // If the animation hasn't finished, repeat the step.
+		  if (progress < 1) requestAnimationFrame(step);
+		};
+	  
+		// Start the animation
+		return step();
+	  };
+	  
+	  animate('x', 0, 1000);
 }
